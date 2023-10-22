a198fcf20326c8706f8b5159ba41bf96d75b571f
Zai Builds <hello@zaibuilds.io>
2023-10-22 19:15:16 +0100

    adding js

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstHeader = document.getElementById('first_heading');
+
+addEventListener(firstHeader, 'click') {
+    function showElement {
+        let introBubble = document.getElementById('intro_div') {
+            
+        }
+    }
+}
