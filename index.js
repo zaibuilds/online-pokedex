382e70ad4ea87d4d4c579c24f3719fa29c9de099
Zai Builds <hello@zaibuilds.io>
2023-10-23 00:56:20 +0100

    animations

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ firstHeader.addEventListener("mouseover", function() {
     container.style.visibility = "visible";
     container.style.transition = 'ease-in-out 800ms'
     container.style.transform = 'ease-in-out'
+    container.style.animation = 'fadeInAnimation ease 3s';
+    container.style.animation.iteration.count = '1';
+    container.style.animation.fill.mode = 'forwards';
 
   });
